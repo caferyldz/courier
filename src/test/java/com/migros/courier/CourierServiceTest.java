597e761e2206025cdb891e871311013da55305f2
caferyildiz <caferyildiz3@gmail.com>
2022-01-29 16:53:34 +0300

    test fixed

--- src/test/java/com/migros/courier/CourierServiceTest.java
+++ src/test/java/com/migros/courier/CourierServiceTest.java
@@ -6,44 +6,31 @@ import com.migros.courier.model.TravelDistanceResponseModel;
 import com.migros.courier.repository.StoreRepository;
 import com.migros.courier.service.CourierServiceImpl;
 import com.migros.courier.service.DistanceCalculatorStrategy;
+import com.migros.courier.service.LocationCalculatorStrategy;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InOrder;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
-import java.time.temporal.TemporalAmount;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 public class CourierServiceTest {
 
-    @Autowired
-    private StoreRepository storeRepository;
-
-    @Autowired
     DistanceCalculatorStrategy distanceCalculatorStrategy;
 
-    private InOrder inOrder;
-
-    @Autowired
     private CourierServiceImpl courierService;
 
     @Before
     public void setUp() {
-        courierService = new CourierServiceImpl(storeRepository, distanceCalculatorStrategy);
+        distanceCalculatorStrategy = new LocationCalculatorStrategy();
+        courierService = new CourierServiceImpl(new StoreRepository(), distanceCalculatorStrategy);
     }
 
     @Test
@@ -54,22 +41,22 @@ public class CourierServiceTest {
         courierRequestModel.setLat(41.055783);
         courierRequestModel.setLng(29.0210292);
         courierRequestModel.setTime(LocalDateTime.now());
+        courierService.courierLocationInput(courierRequestModel);
 
         CourierRequestModel courierRequestModel2 = new CourierRequestModel();
         courierRequestModel2.setCourierId(1L);
         courierRequestModel2.setLat(41.055783);
         courierRequestModel2.setLng(29.0210292);
         courierRequestModel2.setTime(LocalDateTime.now().plusSeconds(30));
+        courierService.courierLocationInput(courierRequestModel2);
 
         CourierRequestModel courierRequestModel3 = new CourierRequestModel();
         courierRequestModel3.setCourierId(1L);
         courierRequestModel3.setLat(41.055783);
         courierRequestModel3.setLng(29.0210292);
         courierRequestModel3.setTime(LocalDateTime.now().plusMinutes(5));
-
-        courierService.courierLocationInput(courierRequestModel);
-        courierService.courierLocationInput(courierRequestModel2);
         courierService.courierLocationInput(courierRequestModel3);
+
 
         List<CourierStoreModel> foundedLocations = courierService.getLocationMap().get(courierRequestModel.getCourierId());
         assertNotNull(foundedLocations);
@@ -77,22 +64,34 @@ public class CourierServiceTest {
     }
 
     @Test
-    public void shouldFinByFollower() {
+    public void shouldGetTotalTravelDistance() {
         CourierRequestModel courierRequestModel = new CourierRequestModel();
         courierRequestModel.setCourierId(1L);
         courierRequestModel.setLat(41.055783);
         courierRequestModel.setLng(29.0210292);
         courierRequestModel.setTime(LocalDateTime.now());
 
-        TravelDistanceResponseModel travelDistanceResponseModel = new TravelDistanceResponseModel();
-        travelDistanceResponseModel.setTotalDistance(20D);
+        CourierRequestModel courierRequestModel2 = new CourierRequestModel();
+        courierRequestModel2.setCourierId(1L);
+        courierRequestModel2.setLat(40.9632463);
+        courierRequestModel2.setLng(29.0630908);
+        courierRequestModel2.setTime(LocalDateTime.now().plusSeconds(5));
 
-        when(courierService.getTotalTravelDistance(courierRequestModel.getCourierId())).thenReturn(travelDistanceResponseModel);
+        CourierRequestModel courierRequestModel3 = new CourierRequestModel();
+        courierRequestModel3.setCourierId(1L);
+        courierRequestModel3.setLat(41.055783);
+        courierRequestModel3.setLng(29.0210292);
+        courierRequestModel3.setTime(LocalDateTime.now().plusMinutes(15));
 
-        final TravelDistanceResponseModel foundTravelDistanceResponseModel = courierService.getTotalTravelDistance(courierRequestModel.getCourierId());
+        courierService.courierLocationInput(courierRequestModel);
+        courierService.courierLocationInput(courierRequestModel2);
+        courierService.courierLocationInput(courierRequestModel3);
+
+        final TravelDistanceResponseModel foundTravelDistanceResponseModel = courierService.getTotalTravelDistance(1L);
 
         assertNotNull(foundTravelDistanceResponseModel);
         assertNotNull(foundTravelDistanceResponseModel.getTotalDistance());
-        assertEquals(travelDistanceResponseModel.getTotalDistance(), foundTravelDistanceResponseModel.getTotalDistance());
+        Double totalDistance = 21756.104252904202;
+        assertEquals(foundTravelDistanceResponseModel.getTotalDistance(), totalDistance);
     }
 }
